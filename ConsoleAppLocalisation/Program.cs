6093b879e0a0f385a289591928d314d4e5b6ce17
Hamzahashmi4444 <77266353+Hamzahashmi4444@users.noreply.github.com>
2021-05-24 16:10:17 +0500

    main

---
+++ ConsoleAppLocalisation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleAppLocalisation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Console App, Localisation. Program starting...");
+
+            //Nothing Yet!!
+
+            Console.WriteLine("Press Enter to Quit.");
+            Console.ReadLine();
+        }
+    }
+}
